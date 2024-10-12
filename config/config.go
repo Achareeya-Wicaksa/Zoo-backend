d436677c2a9d62a10ec2b349bad5535cb3ca93b5
IceManbrz <81310383+IceManbrz@users.noreply.github.com>
2024-10-12 22:44:20 +0700

    ubahan security

--- config/config.go
+++ config/config.go
@@ -4,15 +4,22 @@ import (
     "database/sql"
     "fmt"
     "log"
+    "os"
 
+    "github.com/joho/godotenv"
     _ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
-
+func init() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+}
 func Connect() {
     var err error
-    dsn := "root:@tcp(127.0.0.1:3306)/testbackendzoo"
+    dsn := os.Getenv("DB_CONNECTION_STRING")
     DB, err = sql.Open("mysql", dsn)
     if err != nil {
         log.Fatal(err)
